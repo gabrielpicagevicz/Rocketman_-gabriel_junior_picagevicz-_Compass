724e2bdc376f08ea726c92ff14cd65f9be727094
Gabriel Picagevicz <gabrielpicagevicz15@gmail.com>
2022-07-26 16:24:02 -0300

    Aula Constante

--- aula01/aula01.js
+++ aula01/aula01.js
@@ -1,12 +1,3 @@
-// let idade = 5;
-// console.log(idade);
-// let altura = 180;
-// console.log(altura);
-// camelCase
-
-//Exercicio printar 3 variáveis diferentes.
-let nome = 'Gabriel';
-let segundoNome = 'Junior';
-let sobrenome = 'Picagevicz'
-
-console.log(nome, segundoNome, sobrenome);
+let valorIngressoAdulto = 20;
+valorIngressoAdulto = 30;
+console.log(valorIngressoAdulto);
