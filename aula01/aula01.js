9a3e3f95af9ee9f0686bba3b4467f293d88546ce
Gabriel Picagevicz <gabrielpicagevicz15@gmail.com>
2022-07-26 16:12:22 -0300

    Aula Variaveis e exercicio de printar 3 variaveis

--- aula01/aula01.js
+++ aula01/aula01.js
@@ -1,1 +1,12 @@
-console.log('gabriel')
+// let idade = 5;
+// console.log(idade);
+// let altura = 180;
+// console.log(altura);
+// camelCase
+
+//Exercicio printar 3 variáveis diferentes.
+let nome = 'Gabriel';
+let segundoNome = 'Junior';
+let sobrenome = 'Picagevicz'
+
+console.log(nome, segundoNome, sobrenome);
