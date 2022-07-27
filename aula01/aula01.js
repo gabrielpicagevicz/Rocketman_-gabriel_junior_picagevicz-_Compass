272ddf4020baa82fdae360e062db6dedaa3be1a9
Gabriel Picagevicz <gabrielpicagevicz15@gmail.com>
2022-07-27 19:36:56 -0300

    Aula Objetos]

--- aula01/aula01.js
+++ aula01/aula01.js
@@ -2,4 +2,13 @@ let nome = 'Gabriel'; // string literal
 let idade = 21; // number literal
 let estaAprovado = true; //boolean 
 let sobrenome = undefined; //undefined
-let corSelecionada = null // redefinir um valor NULL;
+
+let pessoa = {
+    nome: 'Gabriel',
+    idade: 21,
+    estaAprovado: true,
+    segundoNome: 'Junior',
+    sobrenome: 'Picagevicz'
+};
+
+console.log(pessoa);
