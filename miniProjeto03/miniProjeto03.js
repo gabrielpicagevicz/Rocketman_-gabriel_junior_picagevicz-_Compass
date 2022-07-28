639109a3f77f69d03c88ddadc830ff6edd540746
Gabriel Picagevicz <gabrielpicagevicz15@gmail.com>
2022-07-28 16:36:53 -0300

    Mini Projeto 03 - FizzBuzz

---
+++ miniProjeto03/miniProjeto03.js
@@ -0,0 +1,23 @@
+// fizzBuzz
+
+function fizzBuzz(entrada){
+    if(typeof entrada !== 'number'){
+        return 'Não é um número'
+    } else { 
+        if(entrada % 3 === 0 && entrada % 5 === 0){
+            return 'FizzBuzz'
+        } else if(entrada % 3 === 0){
+            return 'Fizz'
+        } else if(entrada % 5 === 0){
+            return 'Buzz'
+        } else{
+            return entrada  
+        } 
+    }
+   
+
+}
+
+const resultado = fizzBuzz(1);
+console.log((resultado));
+
