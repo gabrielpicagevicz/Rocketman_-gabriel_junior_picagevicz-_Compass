0824a6f45f29bd9a553ef0f61bbfaf6f8ad4f7dd
Gabriel Picagevicz <gabrielpicagevicz15@gmail.com>
2022-08-01 17:02:01 -0300

    Configuracao mocha-chai e Primeiro Teste

--- mocha-chai/test/sample.spec.js
+++ mocha-chai/test/sample.spec.js
@@ -1,27 +1,25 @@
-import assert from 'assert'
+import assert from "assert";
 
-describe('Meu primeiro teste', () => {
-    it('Verificar uma igualdade', () => {
-    let aux = 2
+describe("Meu primeiro teste", () => {
+  it("Verificar uma igualdade", () => {
+    let aux = 2;
     assert.strictEqual(aux, 2);
-    })
+  });
 
-        it('Verificar outra igualdade', () => {
-            let aux = 'carro'
-            assert.strictEqual(aux, 'carro');
-            })
-})
+  it("Verificar outra igualdade", () => {
+    let aux = "carro";
+    assert.strictEqual(aux, "carro");
+  });
+});
 
-describe('Meu segundo teste', () => {
-    it('Verificar uma desigualdade', () => {
-    let aux = 2
-    assert.notStrictEqual(aux, '2');
-    })
+describe("Meu segundo teste", () => {
+  it("Verificar uma desigualdade", () => {
+    let aux = 2;
+    assert.notStrictEqual(aux, "2");
+  });
 
-        it('Verificar outra desigualdade', () => {
-            let aux = 'carro'
-            assert.notStrictEqual(aux, 'charrete');
-            })
-})
-
-
+  it("Verificar outra desigualdade", () => {
+    let aux = "carro";
+    assert.notStrictEqual(aux, "charrete");
+  });
+});
