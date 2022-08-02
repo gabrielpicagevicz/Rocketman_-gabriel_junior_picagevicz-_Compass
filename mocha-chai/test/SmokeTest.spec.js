c56766e622087bb3886e849437886abdb8a2281b
Gabriel Picagevicz <gabrielpicagevicz15@gmail.com>
2022-08-02 13:40:03 -0300

    Seperado o Smoke Test do codigo da calculadora

---
+++ mocha-chai/test/SmokeTest.spec.js
@@ -0,0 +1,32 @@
+import assert from "assert";
+import chai from "chai";
+import Calculadora from "../src/Calculadora.js";
+
+const expect = chai.expect;
+
+//Smoke Test (testar metodos);
+describe("Smoke Tests - (metodos)", () => {
+  describe("Smoke Tests - Soma", () => {
+    it("Espero que a `soma` seja uma função", () => {
+      expect(Calculadora.soma).to.be.a("function");
+    });
+  });
+
+  describe("Smoke Tests - Subtração", () => {
+    it("Espero que a `sub` seja uma função", () => {
+      expect(Calculadora.sub).to.be.a("function");
+    });
+  });
+
+  describe("Smoke Tests - Multiplicação", () => {
+    it("Espero que a `mult` seja uma função", () => {
+      expect(Calculadora.mult).to.be.a("function");
+    });
+  });
+
+  describe("Smoke Tests - Divisão", () => {
+    it("Espero que a `div` seja uma função", () => {
+      expect(Calculadora.div).to.be.a("function");
+    });
+  });
+});
